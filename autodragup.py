79babdfa80b90c28bd3b736ec1516e1fe5225b8c
dssantos <danilosantos1986@gmail.com>
2021-06-11 23:25:43 -0300

    Refactoring

--- autodragup.py
+++ autodragup.py
@@ -4,14 +4,42 @@ from time import sleep
 import pyautogui as pg
 
 
+def openapp(x, y):
+    pg.moveTo(1123, 707)
+    pg.click()
+    sleep(0.5)
+    pg.moveTo(x, y)
+    pg.click()
+
+def like():
+    if longwait >= 10:
+        pg.moveTo(1176, 338)
+        pg.click()
+        sleep(0.1)
+        pg.click()
+        sleep(shortwait)
+
+def dragup():
+    pg.moveTo(xstart, ystart)
+    sleep(shortwait)
+    pg.drag(0, -460, duration=shortwait)
+
+token = True
+sleep(5)
+openapp(1168, 306)
 while True:
 
-    xstart=1100
-    ystart=450
-    sleep(randrange(30,50))
-
-    pg.moveTo(xstart+randrange(-50, 50), ystart+randrange(-50, 50))
-    pg.drag(0, -200, duration=uniform(0.1, 0.4))
-
-
-
+    xstart = 1150+randrange(-10, 10)
+    ystart = 600+randrange(-2, 2)
+    longwait = randrange(6,15)
+    shortwait = uniform(0.5, 0.9)
+    
+    sleep(longwait)
+    if longwait == 6:
+        token = not token
+        if token:
+            openapp(1168, 306) 
+        else:
+            openapp(1168, 403)
+    like()
+    dragup()
